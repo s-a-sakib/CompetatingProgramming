8a6197ed2f0f0849f9df06e8128b4fc5a58463db
Shahrear Al Sakib <shahrearalsakib16@gmail.com>
2023-03-01 23:20:10 +0600

    CP Code Field

---
+++ test2.cpp
@@ -0,0 +1,23 @@
+#include<iostream> 
+#include<bits/stdc++.h>
+#define int long long int
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define pb push_back
+#define all(X) (X).begin(), (X).end()
+#define np cout << -1 << endl
+#define rn return
+
+using namespace std;
+
+int32_t main(){
+    
+    for(int i = 0 ; i < 15 ; i++){
+        for(int j = 0 ; j < 5 ; j++){
+            if(j == 2) break;
+
+            cout << i << "  " << j << endl;
+        }
+    }
+    return 0;
+}
